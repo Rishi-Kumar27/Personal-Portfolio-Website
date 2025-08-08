bf9eb063213140c7e8d6ab1c51483808025f5de5
Rishi Kumar <rishikumar27203@gmail.com>
2025-08-08 20:06:51 +0530

    google sheet added

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -93,3 +93,16 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+const scriptURL = "https://script.google.com/macros/s/AKfycbxP8lbhLrK1ibN1c2r8vC_1rgc2KcktfmrMQg51BJys96A3_YwLVgFw4Ujc-F4sRJBM/exec";
+
+const form = document.forms["google-sheet"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) =>
+      alert("Thanks for Contacting us..! We Will Contact You Soon...")
+    )
+    .catch((error) => console.error("Error!", error.message));
+});
